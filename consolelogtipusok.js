409c8192884e6924abd98850b886537ff39c33e3
NemethAdam01 <nemeth.adam@diak.szbi-pg.hu>
2025-01-28 09:45:20 +0100

    console log típusok

---
+++ consolelogtipusok.js
@@ -0,0 +1,9 @@
+console.log("Ez egy normál üzenet")
+console.warn("Ez egy figyelmeztetés")
+console.error("Ez egy hiba")
+console.table(
+    [
+        {név : "Anna", kor:25},
+        {név : "Béla", kor:30}
+    ]
+)
